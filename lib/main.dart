abddbd3d0e505944cab648dbb1685af7d1609fb4
Nelson Samayoa <nelson.rs.07@gmail.com>
2019-07-27 09:35:47 -0700

    Implemented webview demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:simple_auth/simple_auth.dart' as simpleAuth;
-import 'package:simple_auth_flutter/simple_auth_flutter.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,25 +10,36 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  initState() {
-    super.initState();
-    SimpleAuthFlutter.init(context);
-  }
-
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
+    return new MaterialApp(
+      title: 'Flutter WebView Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (_) => const MyHomePage(title: 'Flutter WebView Demo'),
+        '/widget': (_) => new WebviewScaffold(
+              url: 'https://www.google.ba',
+              appBar: new AppBar(
+                title: const Text("Ova ye Google"),
+              ),
+              withZoom: true,
+              withLocalStorage: true,
+              hidden: true,
+              initialChild: Container(
+                child: SpinKitDualRing(
+                  color: Colors.blue,
+                  size: 50.0,
+                ),
+              ),
+            ),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
@@ -36,24 +47,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  static String azureClientId = "d0fc0aa6-c341-464c-992f-64e571b9c17a";
-  static String azureTenant = "29fa8797-b4ea-49ca-aa2b-443d88286ed1";
-  final simpleAuth.AzureADV2Api azureApi = new simpleAuth.AzureADV2Api(
-    "azure", //identifier
-    azureClientId, //clientId
-    "native", //clientSecret
-    "https://login.microsoftonline.com/$azureTenant/oauth2/token", //tokenUrl
-    "https://login.microsoftonline.com/$azureTenant/oauth2/authorize", //authUrl
-    "com.example.yakr://redirect"//"https://login.microsoftonline.com/common/oauth2/nativeclient" //redirectUrl
-  );
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,55 +54,29 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RaisedButton(
+            onPressed: () { Navigator.pushNamed(context, '/widget'); },
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child:
+                  const Text('Idemo na Google', style: TextStyle(fontSize: 20)),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          login(azureApi);
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+        ]),
       ),
     );
-  }
-
-  void showError(dynamic ex) {
-    showMessage(ex.toString());
-  }
-
-  void showMessage(String text) {
-    var alert = new AlertDialog(content: new Text(text), actions: <Widget>[
-      new FlatButton(
-          child: const Text("Ok"),
-          onPressed: () {
-            Navigator.pop(context);
-          })
-    ]);
-    showDialog(context: context, builder: (BuildContext context) => alert);
-  }
-
-  void login(simpleAuth.AzureADV2Api api) async {
-    try {
-      var success = await api.authenticate();
-      showMessage("Logged in success: $success");
-    } catch (e) {
-      showError(e);
-    }
-  }
-
-  void logout(simpleAuth.AzureADV2Api api) async {
-    await api.logOut();
-    showMessage("Logged out");
   }
 }
