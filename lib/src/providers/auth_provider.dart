7f988ca8d4369551d34e110c4425b4138e6f0e54
Nelson Samayoa <nelson.rs.07@gmail.com>
2019-07-27 13:47:20 -0700

    Cleanup and structuring

---
+++ lib/src/providers/auth_provider.dart
@@ -0,0 +1,32 @@
+import 'package:aad_oauth/aad_oauth.dart';
+import 'package:aad_oauth/model/config.dart';
+
+class AuthProvider {
+  static String azureClientId = "d0fc0aa6-c341-464c-992f-64e571b9c17a";
+  static String azureTenant = "29fa8797-b4ea-49ca-aa2b-443d88286ed1";
+
+  static final Config config = new Config(
+    azureTenant,
+    azureClientId,
+    "openid profile offline_access",
+    "https://login.live.com/oauth20_desktop.srf",
+  );
+
+  // TODO: This needs to be set at some point
+  // var screenSize = MediaQuery.of(context).size;
+  // var rectSize =  Rect.fromLTWH(0.0, 25.0, screenSize.width, screenSize.height - 25);
+  // oauth.setWebViewScreenSize(rectSize);
+
+  final AadOAuth oauth = AadOAuth(config);
+
+  void login() async {
+    try {
+      await oauth.login();
+      String accessToken = await oauth.getAccessToken();
+    } catch (e) {}
+  }
+
+  void logout() async {
+    await oauth.logout();
+  }
+}
