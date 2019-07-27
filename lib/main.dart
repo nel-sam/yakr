5e14f9b50669c6b3a32f255f1a7ed07316b01fdf
Nelson Samayoa <nelson.rs.07@gmail.com>
2019-07-27 14:27:17 -0700

    Fixes and made material

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'package:yakr/src/ui/home.dart';
 
 void main() {
   runApp(App());
+}
+
+class App extends StatelessWidget {
+  final appTitle = 'Yakr';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      home: Home(
+        title: appTitle,
+      ),
+    );
+  }
 }
