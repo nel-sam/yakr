ebbae02a88bb4a0fc004b401acdb7a7e9a8af0a8
Nelson Samayoa <nelson.rs.07@gmail.com>
2019-07-30 07:07:54 -0700

    Simplified BLoC implementation

--- lib/src/blocs/theme_state.dart
+++ lib/src/blocs/theme_state.dart
@@ -1,13 +1,10 @@
 import 'package:yakr/src/ui/yakr_themes.dart';
-import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
-@immutable
-class ThemeState extends Equatable {
+class ThemeState {
   String selectedThemeKey;
 
   ThemeState._();
-  ThemeState([List props = const []]) : super(props);
+  ThemeState([List props = const []]);
 
   factory ThemeState.initial() {
     return ThemeState._()..selectedThemeKey = YakrThemes.defaultTheme;
